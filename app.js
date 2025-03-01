f999afb3e576a57bfad74b17df8970515e26edcb
kuldeep407 <kuldeepchahar426@gmail.com>
2025-03-02 00:32:21 +0530

    update

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path"
+import path from "path";
 import cors from "cors";
-
 
 import { connectDB } from "./config/db.js";
 import userRoute from "./routes/userRoute.js";
-import folderRotue from './routes/folderRoute.js'
-import imageRoute from './routes/imageRoute.js'
+import folderRotue from "./routes/folderRoute.js";
+import imageRoute from "./routes/imageRoute.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join("uploads")));
@@ -22,7 +29,6 @@ app.use("/uploads", express.static(path.join("uploads")));
 app.use(userRoute);
 app.use(folderRotue);
 app.use(imageRoute);
-
 
 app.get("/", (req, res) => {
   console.log("Server is running...");
